97fcaa8e224e110de2d497017aca978f3060855d
Omar Khankan <omar.khankan@gmail.com>
2023-04-16 02:35:13 +0300

    fixing pages

--- routes/pagesRoute.js
+++ routes/pagesRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route("/getallpages").get(PagesControllers.getAllPages);
 router.route("/getallpagesbyid/:id").get(PagesControllers.getPage);
 router.route("/createpage").post(upload.single('image'), PagesControllers.postPage);
-router.route("/updatepage/:id").put(PagesControllers.updatePage);
+router.route("/updatepage/:id").put(upload.single('image'),PagesControllers.updatePage);
 router.route("/deletepage/:id").delete(PagesControllers.erasePage);
 
 
