ebcc43ed2c32b99a3be2e8122b94d23d717ef3cb
Maya <maya.atiah.99@outlook.com>
2023-05-05 22:03:53 +0300

    user getAll

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -15,7 +15,7 @@ router.post('/register', registerUser)
 router.route('/login').post(loginUser)
 router.route('/logout').post(logoutUser)
 router.route('/delete/:id').delete(deleteUser)
-router.get('/getall', isAuthenticated, getAll);
+router.get('/getall', getAll);
 router.get('/me/:id', isAuthenticated, getMe);
 router.get('/auth/user-role', isAuthenticated, (req, res) => {
     res.status(200).json({ role: req.user.role });
